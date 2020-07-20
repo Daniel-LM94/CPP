845bea57c219390f0f03e75a011b4a26d461dc8e
Daniel Lainez <eduardlainez@hotmail.com>
2020-07-20 16:03:24 -0600

    Cambio 1

--- Ejercicio 8 - Factura con Descuento/FacturaConDescuento.cpp
+++ Ejercicio 8 - Factura con Descuento/FacturaConDescuento.cpp
@@ -7,15 +7,20 @@ int main(int argc, char const *argv[])
     double Subtotal = 0; 
     double Total = 0;
     double Impuesto = 0.5;
+    int Descuento = 0;
+    double CalDescuento = 0; //Calculo del Descuento
+    double CalImpuesto = 0; //Calculo del Impuesto
 
+    
   //Proceso
     cout << "\n Ingrese el Subtotal de la Factura"<<endl;
     cin >> Subtotal;
+    cout << "\n Ingrese el Descuento (0, 10, 15, 20)"<<endl; 
+    cin >> Descuento;
+    
+  //Salida
     Total = Subtotal+(Subtotal*Impuesto);
     cout << endl;
-
-  //Salida
     cout << "Total a pagar es: " <<Total;
     return 0;
 }
-
