41933e1943ba15b058a498bc5598dc291b18a3ed
Daniel Lainez <eduardlainez@hotmail.com>
2020-07-26 15:24:55 -0600

    Ejercicio 3 - Variable

---
+++ Ejercicio 3 - Variables/Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   int NumEntero = 15;
+
+   cout << "Numero entero = [" << NumEntero << "] \n";
+      
+   NumEntero = 18;
+
+   cout << "Numero entero modificado = [" << NumEntero << "] \n";
+      
+   return 0;
+}
