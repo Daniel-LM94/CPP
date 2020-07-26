1bcaf3c3cb52734606ff7699a18ada0be8f01337
Daniel Lainez <eduardlainez@hotmail.com>
2020-07-26 14:53:04 -0600

    Ejercicio 2 - Mensages

---
+++ Ejercicio 2 - Mensages/Mensajes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   string Nombre;
+   cout << "Ingrese su nombre";
+   cin  >> Nombre;
+   cout << endl;
+   cout << "Hola mi nombre es: " <<Nombre;
+   cout << endl;
+   
+    return 0;
+}
