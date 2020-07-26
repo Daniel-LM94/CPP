25a7267ab45c7a9c5a39e92771d0c2ba74e91bfd
Daniel Lainez <eduardlainez@hotmail.com>
2020-07-26 17:56:12 -0600

    Ejercicio 5 - Programa Suma

---
+++ Ejercicio 5 - Programa Suma/ProgramaSuma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+ int a = 0; int b; int resultado = 0; 
+ cout << "Ingrese el Valor de a :" << endl;
+ cin >> a;
+
+ cout << endl;
+ cout << "Ingrese el Valor de b :" << endl;
+ cin >> b;
+
+ resultado = a+b;
+ cout << "La Suma de a + b es :" << resultado;
+ return 0;
+}
+ 
