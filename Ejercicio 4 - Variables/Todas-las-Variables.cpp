b09a546701046ea212c6057ebf00da5358257d78
Daniel Lainez <eduardlainez@hotmail.com>
2020-07-26 16:04:38 -0600

    Ejercicio 4 - Todas las Variables

---
+++ Ejercicio 4 - Variables/Todas-las-Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   bool ValorBoolean  = true;
+   int ValorEntero    = 15;
+   double ValorDouble = 20.99;
+   string ValorString = "Hola como estan?";
+   char ValorChar     = 'H';
+
+   cout << "Valor Boolean = [" << ValorBoolean << "] \n";
+   cout << "Valor Entero  = [" << ValorEntero << "] \n";
+   cout << "Valor Double  = [" << ValorDouble << "] \n";
+   cout << "Valor String  = [" << ValorString << "] \n";
+   cout << "Valor Char    = [" << ValorChar << "] \n";
+     
+   return 0;
+}
