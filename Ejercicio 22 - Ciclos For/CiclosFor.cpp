2bd4d1396e5c9748ff8e99872bf6d1fbae713590
Daniel Lainez <eduardlainez@hotmail.com>
2020-07-27 21:06:18 -0600

    Ejercicio 22 - Ciclos For

---
+++ Ejercicio 22 - Ciclos For/CiclosFor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{ 
+    for (int indice = 1; indice <= 10; indice ++)
+    {
+        cout << "Valor de la Variable Indice" << indice << endl;
+    }
+
+    return 0;
+}
