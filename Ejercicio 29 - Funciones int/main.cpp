fbc3d3476205cb4367ecba465bfc3db0bd2b2319
Daniel Lainez <eduardlainez@hotmail.com>
2020-08-16 13:37:52 -0600

    Ejercicio 29 - Finciones int

---
+++ Ejercicio 29 - Funciones int/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int SUMAR (int a, int b) 
+{ 
+    return a + b;
+}
+
+int main(int argc, char const *argv[])
+{
+    system ("cls");
+    cout << SUMAR(5, 2) << endl;
+    cout << endl;   
+    cout << SUMAR(15, 2) << endl;
+    cout << endl;   
+    cout << SUMAR(5, 71) << endl;
+    cout << endl;   
+    cout << SUMAR(9, 8) << endl;
+    cout << endl;     
+        
+    return 0;
+}
